0f535ff07da24eeca2ef6262d8ff989fc846e4a8
WASDetchan <yarik.antipov2008@gmail.com>
2025-08-12 16:52:09 +0300

    Create graphics pipeline

--- dayl/src/main.rs
+++ dayl/src/main.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::sync::Arc;
 
-use wknup::vk::pipeline::PipelineManager;
-use wknup::vk::shader::{ShaderStage, ShaderStageInfo};
-use wknup::window::WindowManager;
+use wknup::vk::pipeline::GraphicsPipelineBuilder;
+use wknup::{
+    vk::shader::{ShaderModule, ShaderStage, ShaderStageInfo},
+    window::WindowManager,
+};
 const SHADER_BYTES: &[u8; include_bytes!(env!("shader.spv")).len()] =
     include_bytes!(env!("shader.spv"));
 const SHADER: [u32; SHADER_BYTES.len() / 4] = unsafe { std::mem::transmute_copy(SHADER_BYTES) };
@@ -16,15 +18,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 #[tokio::main]
 async fn start(window: &WindowManager) -> Result<(), Box<dyn Error>> {
-    println!(env!("shader.spv"));
-    let mut vk_manager = wknup::vk::VulkanManager::init(window)?;
-    vk_manager.create_swapchain()?;
-    let shader = Arc::new(vk_manager.create_shader_module(&SHADER));
+    // println!(env!("shader.spv"));
+    let vulkan = Arc::new(wknup::vk::VulkanBuilder::new(window).build()?);
+    let device = vulkan.get_device();
+    let swapchain_manager = vulkan.get_swapchain_manager();
+    let swapchain = Arc::new(swapchain_manager.create_swapchain()?);
+
+    let shader = Arc::new(ShaderModule::new(Arc::clone(&device), &SHADER));
     let vertex_info = ShaderStageInfo::new(shader.clone(), ShaderStage::Vertex, "main_vs".into());
     let fragment_info =
         ShaderStageInfo::new(shader.clone(), ShaderStage::Fragment, "main_fs".into());
-    let mut pipeline = PipelineManager::init(&vk_manager);
-    pipeline.add_stage("vertex".into(), vertex_info);
-    pipeline.add_stage("fragment".into(), fragment_info);
+    let pipeline = GraphicsPipelineBuilder::new(vulkan, swapchain)
+        .add_stage("vertex".into(), vertex_info)
+        .add_stage("fragment".into(), fragment_info)
+        .build()?;
     Ok(())
 }
